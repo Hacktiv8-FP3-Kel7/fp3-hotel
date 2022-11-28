bd5a6d0c40d8fded9ec080f988da90518d3ba91b
Jems K <jameskirawan@gmail.com>
2022-11-28 21:50:03 +0700

    add mutation fetch function helper

---
+++ src/common/helpers/common.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+type MutationMethodType = "GET" | "POST";
+export function MutationFetchFunction(
+  url: string,
+  method: MutationMethodType,
+  body?: any
+): Promise<any> {
+  return new Promise(async (resolve, reject) => {
+    try {
+      resolve(
+        await axios({
+          method: method,
+          url: url,
+          data: body,
+        })
+      );
+    } catch (e: any) {
+      reject("Error");
+    }
+  });
+}
