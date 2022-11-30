e8bbeb32be2e142fa9034fd1c8e6baca2d3a0862
ferdy <ferdy.lim@springkraf.com>
2022-11-30 20:34:27 +0700

    fix: home-screen

--- src/screens/home-screen.tsx
+++ src/screens/home-screen.tsx
@@ -39,8 +39,7 @@ export default function HomeScreen(props: Props) {
         keyExtractor={(item) => item.hotelId}
       />
 
-        <Home />
-      </View>
-    </>
+      <Home />
+    </View>
   );
 }
